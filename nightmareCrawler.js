4b1b9c294a3ff373255da535ac733d7e5c0456af
Rowen Wu <Rowen Wu>
2017-02-11 23:49:48 -0800

    crawler works with single unit, logs store name to console

--- nightmareCrawler.js
+++ nightmareCrawler.js
@@ -3,10 +3,12 @@ var nightmare = Nightmare({ show: true })
 
 nightmare
   .goto('https://www.thecrossroadsmall.com/en/directory/map.html')
-  .wait('.view-switcher__button')
-  .click('.view-switcher__button')
+  .wait('.Units')
+  .click('.Units')
+  .wait('.pop-up-window-container')
   .evaluate(function () {
-      return document.querySelector('.view-switcher__button').textContent;
+      var child = document.querySelector('.store-detail').firstElementChild;
+      return child.textContent;
   })
   .end()
   .then(function (result) {
